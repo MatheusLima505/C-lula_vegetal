ec436cdb440bcf81dc0af0b0d4138cf933f580a7
MatheusLima505 <mathlegeferi@gmail.com>
2025-06-12 00:23:34 -0300

    fodae

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,12 +1,24 @@
 import pages from './../assets/pages.json';
 import './../home.css'; // importar css
 
+
+
 function Content({ pindex }) {
   const spage = pages[pindex];
+  function Image({img}) {
+    if (spage.img!='') {
+      return (<img src={spage.img} alt="" className='content-image' />)
+    }
+  }
+
   return (
     <div className="content-container">
       <h2 className="content-title">{spage.title}</h2>
-      <p className="content-text">{spage.text}</p>
+      <Image />
+      {spage.text.split('\n\n').map((paragraph, i) => (
+        <p key={i} style={{ marginBottom: '1em' }} className='content-text'>{paragraph}</p>
+      ))}
+      
     </div>
   );
 }
